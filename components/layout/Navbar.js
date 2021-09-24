2484f45dedc2e6aea2d5bcbddae74f07d50289ea
ScorchingShade <ankushors789@gmail.com>
2021-09-24 06:00:51 +0530

    added fontawesome and bars to responsive nav

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Link from "next/link";
-import classes from "./navbar.module.css";
 
+import classes from "./navbar.module.css";
 
 function Navbar() {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}> 
+      <div className={classes.logo}>
         <Link href="/">Plano</Link>
       </div>
       <nav className={classes.navigation}>
         <ul>
           <li>
-            <Link href="/" >API</Link>
+            <Link href="/">API</Link>
           </li>
           <li>
             <Link href="/">Dashboard</Link>
@@ -21,8 +21,15 @@ function Navbar() {
             <Link href="/">Planners</Link>
           </li>
         </ul>
-        <button type="button" className={classes.callToAction}>Sign in</button>
+        <button type="button" className={classes.callToAction}>
+          Sign in
+        </button>
+        <div className={classes.bars}>
+             <i className="fa fa-bars"></i>
+        </div>
+       
       </nav>
+     
     </header>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import Layout from '../components/layout'
 import '../styles/globals.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+
 
 function MyApp({ Component, pageProps }) {
   return <Layout><Component {...pageProps} /></Layout>
