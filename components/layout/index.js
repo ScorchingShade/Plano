c42edfa35b4181f9bad3fcf2a7c8b332eaff1e4c
ScorchingShade <ankushors789@gmail.com>
2021-09-24 16:27:40 +0530

    added footer

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from "react";
+import Footer from "./footer";
 import Navbar from "./navbar";
+import classes from "./layout.module.css"
 
 function Layout(props) {
   return (
     <Fragment>
       <Navbar />
-      <main>{props.children}</main>
+      <main className={classes.mainbody}>{props.children}</main>
+      <Footer/>
     </Fragment>
   );
 }
