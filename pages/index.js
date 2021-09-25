f2dc09899cdd4754bf1eb389d9d9151f9687a33b
ScorchingShade <ankushors789@gmail.com>
2021-09-25 17:07:33 +0530

    added homepage design till caption1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
-
+import { Fragment } from "react";
+import classes from "./homepage.module.css";
+import illust from "../public/images/home_graphic.png";
+import Image from "next/image";
+import Button from "../ui/button";
 
 export default function Home() {
   return (
-   <div>
-     Hello
-     <button>Click</button>
-   </div>
-  )
+    <Fragment>
+      <div className={classes.body}>
+        <div className={classes.box_bg}></div>
+        <div className={classes.caption1}>
+          <p>Do you often fail to make decisions daily?<br/><br/>
+
+We did too! That’s why we built an app that could do it for us!</p>
+        </div>
+
+        <div className={classes.illus}>
+          <Image
+            src={illust}
+            alt="illustration"
+            width={700}
+            height={250}
+            className={classes.illusImage}
+          />
+          <div className={classes.btnAlign}>
+            <button className={classes.btn}>Start deciding today</button>
+          </div>
+        </div>
+
+        <div className={classes.bigBox}></div>
+      </div>
+    </Fragment>
+  );
 }
