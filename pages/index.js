e913ef30654b680bdd08f4265a1e4a5f6643bc81
ScorchingShade <ankushors789@gmail.com>
2021-09-25 17:58:27 +0530

    completed desktop UI/ pending button active

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,12 @@ export default function Home() {
       <div className={classes.body}>
         <div className={classes.box_bg}></div>
         <div className={classes.caption1}>
-          <p>Do you often fail to make decisions daily?<br/><br/>
-
-We did too! That’s why we built an app that could do it for us!</p>
+          <p>
+            Do you often fail to make decisions daily?
+            <br />
+            <br />
+            We did too! That’s why we built an app that could do it for us!
+          </p>
         </div>
 
         <div className={classes.illus}>
@@ -29,6 +32,25 @@ We did too! That’s why we built an app that could do it for us!</p>
         </div>
 
         <div className={classes.bigBox}></div>
+        <div className={classes.caption2}>
+          <p>
+            Plano learns from you!Our curated behaviour engine picks cues from
+            your mood and understands the best course of action that you should
+            take
+          </p>
+        </div>
+
+        <div className={classes.caption3}>
+          <p>
+            Plano is a decision making and planning app that takes over the
+            tedious task of making choices and plans for you!
+          </p>
+          <p>Interested in building plano? Join us today!</p>
+
+          <div className={classes.btnAlignDev}>
+            <button className={classes.btn}>Develop with us</button>
+          </div>
+        </div>
       </div>
     </Fragment>
   );
